2c658d43da856a2b1b64427a01080e716eec509c
Devaing Group <git@devaing.net>
2024-10-06 13:41:33 +0100

    dv

---
+++ src/Controllers/PurchaseVerificationController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Devaing\Envato\Controllers;
+
+use Illuminate\Routing\Controller;
+use Devaing\Envato\Traits\AppBoot;
+
+class PurchaseVerificationController extends Controller
+{
+   use AppBoot;
+}
